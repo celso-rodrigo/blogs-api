ab9ea9914543c47770b69caf41a8aa907fd5a344
celsorodrigo <celsorodrigoma@gmail.com>
2022-10-27 14:35:59 -0300

    Concluio 15° requisito.

--- src/middlewares/validatePostInputs.js
+++ src/middlewares/validatePostInputs.js
@@ -1,9 +1,15 @@
 const MISSING_FIELDS = { message: 'Some required fields are missing' };
 
 const validatePostInputs = (req, res, next) => {
-  const { title, content, categoryIds } = req.body;
-  if (!title || !content || !categoryIds) return res.status(400).json(MISSING_FIELDS);
+  const { title, content } = req.body;
+  if (!title || !content) return res.status(400).json(MISSING_FIELDS);
   next();
 };
 
-module.exports = { validatePostInputs };
+const validatePostCategory = (req, res, next) => {
+  const { categoryIds } = req.body;
+  if (!categoryIds) return res.status(400).json(MISSING_FIELDS);
+  next();
+};
+
+module.exports = { validatePostInputs, validatePostCategory };
